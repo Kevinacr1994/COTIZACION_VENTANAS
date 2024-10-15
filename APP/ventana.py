45f2a79fb89ab606f106aa07cb276725aa27c16d
KEVIN CUBIDES <kevinandrea04@gmail.com>
2024-10-15 16:15:57 -0500

    se modificaciones en la clase ventana

--- APP/ventana.py
+++ APP/ventana.py
@@ -14,14 +14,14 @@ class Ventana:
     PRECIO_CHAPA = 16200
     PRECIO_ESQUINA = 4310
 
-def __init__(self, ancho, alto, estilo, vidrio, esmerilado=False):
+    def __init__(self, ancho, alto, estilo, vidrio, esmerilado=False):
         self.ancho = ancho
         self.alto = alto
         self.estilo = estilo
         self.vidrio = vidrio
         self.esmerilado = esmerilado
 
-def calcular_costo(self):
+    def calcular_costo(self):
         area_vidrio = (self.ancho - 3) * (self.alto - 3)  # Descontando bordes de 1.5cm por cada lado
         costo_vidrio = area_vidrio * self.PRECIOS_VIDRIO[self.vidrio]
         if self.esmerilado:
@@ -38,6 +38,6 @@ def calcular_costo(self):
 
         return costo_total
 
-def __str__(self):
+    def __str__(self):
         return (f"Ventana {self.estilo}, {self.ancho}x{self.alto} cm, vidrio {self.vidrio} "
                 f"{'esmerilado' if self.esmerilado else ''}. Costo calculado: {self.calcular_costo():.2f}")
