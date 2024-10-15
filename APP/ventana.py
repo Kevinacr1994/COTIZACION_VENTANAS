6d97d9ee571eee8bcbf13853a721f6cf0e29c15d
KEVIN CUBIDES <kevinandrea04@gmail.com>
2024-10-15 14:39:51 -0500

    se crea la clase ventana

---
+++ APP/ventana.py
@@ -0,0 +1,15 @@
+class Ventana:
+    PRECIOS_ACABADO = {
+        'Pulido': 50700,
+        'Lacado Brillante': 54200,
+        'Lacado Mate': 53600,
+        'Anodizado': 57300
+    }
+    PRECIOS_VIDRIO = {
+        'Transparente': 8.25,
+        'Bronce': 9.15,
+        'Azul': 12.75
+    }
+    PRECIO_ESMERILADO = 5.20
+    PRECIO_CHAPA = 16200
+    PRECIO_ESQUINA = 4310
